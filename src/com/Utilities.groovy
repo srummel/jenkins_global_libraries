8c8b0fa50f15265d01f8b92c4115c61eaec0c6b4
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 15:33:06 -0600

    added mkdir to external method

--- src/com/Utilities.groovy
+++ src/com/Utilities.groovy
@@ -1,7 +1,9 @@
 
 /// Methods in this file will end up as object methods on the object that load returns.
-static def lookAtThis(String whoAreYou) {
-   echo("Look at this, ${whoAreYou}! You loaded this from another file!")
+static def lookAtThis(String whom) {
+   echo "hello ${whom}"
+   mkdir tmp
+   echo "look for a new dir"
 }
 
 return this;
