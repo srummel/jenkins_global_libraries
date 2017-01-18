3165ba03494e4bb13a10de8d3f36d74330925ffb
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 15:34:04 -0600

    paired down external command

--- src/com/Utilities.groovy
+++ src/com/Utilities.groovy
@@ -2,8 +2,6 @@
 /// Methods in this file will end up as object methods on the object that load returns.
 static def lookAtThis(String whom) {
    echo "hello ${whom}"
-   mkdir tmp
-   echo "look for a new dir"
 }
 
 return this;
