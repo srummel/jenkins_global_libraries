338f84233f9efa8aa7e38402a830d41ba8672129
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 11:04:42 -0600

    added debug lines

--- org/foo/Utilities.groovy
+++ org/foo/Utilities.groovy
@@ -2,6 +2,7 @@ package org.foo
 
 class Utilities {
   static def mvn(script, args) {
-    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
+  println("IN UTILITIES")
+//    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
   }
 }
