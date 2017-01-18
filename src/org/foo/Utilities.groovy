e6fed2dbb44eb835945fb301822e9b41dc25de8a
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 10:05:39 -0600

    added annotation to global code

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,4 +1,6 @@
 package org.foo
+
+@Library('utils')
 class Utilities {
   static def mvn(script, args) {
     script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
