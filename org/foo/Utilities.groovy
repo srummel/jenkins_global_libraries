8af09dc9c0b44ba9f796ab7cf68eee9462a103a8
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 12:13:10 -0600

    re-configured external groovy code

--- org/foo/Utilities.groovy
+++
@@ -1,8 +0,0 @@
-package org.foo
-
-class Utilities {
-  static def mvn(script, args) {
-  println("IN UTILITIES")
-//    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
-  }
-}
