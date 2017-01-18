3e62e8bf9c4cbf4ec7fb7f88ec93fc9fa735d9f5
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 16:14:00 -0600

    quotes

--- src/com/Utilities.groovy
+++ src/com/Utilities.groovy
@@ -2,7 +2,7 @@ package com
 
 /// Methods in this file will end up as object methods on the object that load returns.
 static def hello(String whom) {
-   echo "hello ${whom}"
+   echo 'hello ${whom}'
 }
 
 return this;
